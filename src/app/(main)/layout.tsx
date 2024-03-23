3532c9142294d50bd20b6d30040caed8b9a6c941
Ego Maragustaf <egomaragustaf@gmail.com>
2024-03-24 04:54:16 +0700

    feat: fix layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -8,14 +8,15 @@ import { Pricing } from "./_components/section-pricing";
 import { Contact } from "./_components/section-contact";
 
 export default function MainLayout({
-  className,
   children,
-}: React.HTMLAttributes<HTMLElement>) {
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <div className="w-full mx-auto flex min-h-screen flex-col px-2">
       <HeroBackground />
       <Header />
-      <main className={cn("flex-1", className)}>{children}</main>
+      <main className="flex-1">{children}</main>
       <About />
       <Pricing />
       <Contact />
