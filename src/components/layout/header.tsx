7b6d287a12bbe16321ce263daa63c925d5fb4225
Ego Maragustaf <egomaragustaf@gmail.com>
2024-03-24 03:15:28 +0700

    feat: add backdrop blur navigation when scroll

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,14 +1,34 @@
 "use client";
 
 import Link from "next/link";
+import { IconCodeCircle2 } from "@tabler/icons-react";
 
+import useScroll from "@/hooks/use-scroll-position";
 import { Button } from "@/components/ui/button";
 import { navigation } from "@/configs/navigation";
+import { cn } from "@/lib/utils";
 
 export function Header() {
+  const scrolled = useScroll(20);
+
   return (
-    <nav className="sticky top-0 z-40 items-center justify-between gap-2 py-8 transition-colors lg:flex">
-      <span className="flex items-center text-xl font-bold">HOME</span>
+    <nav
+      className={cn(
+        "sticky top-0 z-40 items-center justify-between gap-2 py-6 transition-colors lg:flex",
+        { "bg-background/75 backdrop-blur-lg": scrolled }
+      )}>
+      <Link href="/">
+        <span
+          className={cn(
+            "flex gap-2 items-center text-xl font-bold text-white",
+            {
+              "text-black": scrolled,
+            }
+          )}>
+          <IconCodeCircle2 />
+          Home
+        </span>
+      </Link>
       <ul className="hidden lg:flex items-center gap-14">
         {navigation.map((item) => (
           <li key={item.name} className="text-xl font-bold">
