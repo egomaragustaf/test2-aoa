4d01afe04e165abe8f469c75fd36a7c3fe87b055
Ego Maragustaf <egomaragustaf@gmail.com>
2024-03-24 03:23:48 +0700

    feat: set buttom login color

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -12,7 +12,7 @@ export default function MainLayout({
   children,
 }: React.HTMLAttributes<HTMLElement>) {
   return (
-    <div className="max-w-screen-xl mx-auto flex min-h-screen flex-col px-2">
+    <div className="w-full mx-auto flex min-h-screen flex-col px-2">
       <HeroBackground />
       <Header />
       <main className={cn("flex-1", className)}>{children}</main>
